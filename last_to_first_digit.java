ae1d89bcb44e8528d42abd49834e10de4239e025
Shubham Solanki <shubham24071999@gmail.com>
2025-09-11 16:48:02 +0530

    Create last_to_first_digit.java

---
+++ last_to_first_digit.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc= new Scanner(System.in);
+		int n = sc.nextInt();
+		
+         while(n>0){
+             int dig = n%10;
+             n =n/10;
+             System.out.println(dig);
+         }
+		}
+
+	}
+}
