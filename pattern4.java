ae923b3b57e7cbdf704f7b03e0074dea6ffca189
Shubham Solanki <shubham24071999@gmail.com>
2025-09-12 23:34:15 +0530

    Create pattern5.java

---
+++ pattern4.java
@@ -0,0 +1,31 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+        Scanner sc =new Scanner(System.in);
+        int n = sc.nextInt();
+        int sp = n/2;
+        int st = 1;
+        for(int i =1;i<=n;i++){
+            for(int j =1;j<=sp;j++){
+                System.out.print("\t");
+            }
+            for(int j =1;j<=st;j++){
+                System.out.print("*\t");
+            }
+            if(i<=n/2){
+                sp--;
+                st+=2;
+            }else{
+                sp++;
+                st-=2;
+            }
+            System.out.println();
+        }
+	}
+}
