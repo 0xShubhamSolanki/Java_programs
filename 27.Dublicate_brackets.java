2b53ebd2dce3ab4ca723aecbc8b04d476cec2097
Shubham Solanki <shubham24071999@gmail.com>
2025-09-22 18:59:55 +0530

    Create 27.Dublicate_brackets.java

---
+++ 27.Dublicate_brackets.java
@@ -0,0 +1,32 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc = new Scanner(System.in);
+		String str = sc.nextLine();
+		
+		Stack<Character> st = new Stack<>();
+		for(int i =0;i<str.length();i++){
+		    char ch =str.charAt(i);
+		    if(ch==')'){
+		        if(st.peek() =='('){
+		            System.out.println(true);
+                    return;		      
+		        }else{
+		            while(st.peek()!='('){
+		                st.pop();
+		            }
+		            st.pop();
+		        }
+		    }else{
+		        st.push(ch);
+		    }
+		} 
+     System.out.println(false);
+	}
+}
